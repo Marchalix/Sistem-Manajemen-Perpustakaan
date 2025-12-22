e5d20e8073a266f84f8a71adbffe70678b62cf70
unknown <residencezmarvel@gmail.com>
2025-12-23 06:23:47 +0700

    Add loan data with fine calculation

--- src/main/java/org/example/data/BookData.java
+++ src/main/java/org/example/data/BookData.java
@@ -1,4 +1,30 @@
 package org.example.data;
 
 public class BookData {
+    public String kode;
+    public String judul;
+    public String pengarang;
+    public int tahun;
+    public int stockTersedia;
+    public String status; // tersedia / dipinjam
+    public String coverPath;
+
+    public String toCSV() {
+        return kode + "," + judul + "," + pengarang + "," + tahun + "," +
+                stockTersedia + "," + status + "," + coverPath;
+    }
+
+    public static BookData fromCSV(String line) {
+        String[] d = line.split(",");
+        BookData b = new BookData();
+        b.kode = d[0];
+        b.judul = d[1];
+        b.pengarang = d[2];
+        b.tahun = Integer.parseInt(d[3]);
+        b.stockTersedia = Integer.parseInt(d[4]);
+        b.status = d[5];
+        b.coverPath = d[6];
+        return b;
+    }
 }
+
