d2a0430e815e8e78d387ffbaec9c1ec2bd9a504c
kelakmati <maarlove654@gmail.com>
2025-12-23 05:55:16 +0700

    Create login GUI with validation popup

--- src/main/java/org/example/gui/LoginFrame.java
+++ src/main/java/org/example/gui/LoginFrame.java
@@ -1,4 +1,39 @@
 package org.example.gui;
 
-public class LoginFrame {
+import javax.swing.*;
+import java.awt.event.*;
+
+
+public class LoginFrame extends JFrame {
+    JTextField tfId = new JTextField();
+    JPasswordField pfPass = new JPasswordField();
+
+    public LoginFrame() {
+        setTitle("Login Perpustakaan");
+        setSize(350, 250);
+        setLayout(null);
+
+        JLabel l1 = new JLabel("ID");
+        JLabel l2 = new JLabel("Password");
+        JButton btn = new JButton("Login");
+
+        l1.setBounds(40,40,100,25);
+        tfId.setBounds(140,40,150,25);
+        l2.setBounds(40,80,100,25);
+        pfPass.setBounds(140,80,150,25);
+        btn.setBounds(120,130,100,30);
+
+        add(l1); add(tfId);
+        add(l2); add(pfPass);
+        add(btn);
+
+        btn.addActionListener(e -> {
+            if (tfId.getText().isEmpty()) {
+                JOptionPane.showMessageDialog(this, "ID tidak boleh kosong");
+            }
+        });
+
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
+    }
 }
