74d4ecec9fcfaf3e40b42e44293737a0e36d3a7d
kelakmati <maarlove654@gmail.com>
2025-12-23 20:12:00 +0700

    Add book display with cover image

--- src/main/java/org/example/gui/BookPanel.java
+++ src/main/java/org/example/gui/BookPanel.java
@@ -1,4 +1,25 @@
 package org.example.gui;
 
-public class BookPanel {
+import org.example.data.BookData;
+import org.example.data.BookManager;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class BookPanel extends JPanel {
+    public BookPanel() {
+        setLayout(new GridLayout(0,3,10,10));
+
+        for (BookData b : BookManager.getAll()) {
+            JPanel card = new JPanel(new BorderLayout());
+            card.setBorder(BorderFactory.createLineBorder(Color.GRAY));
+
+            JLabel img = new JLabel(new ImageIcon(b.coverPath));
+            JLabel title = new JLabel(b.judul, SwingConstants.CENTER);
+
+            card.add(img, BorderLayout.CENTER);
+            card.add(title, BorderLayout.SOUTH);
+            add(card);
+        }
+    }
 }
