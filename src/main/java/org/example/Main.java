c9616de5f5eeeb9abcf9769b061e523c0a22c29f
unknown <residencezmarvel@gmail.com>
2025-12-24 17:17:57 +0700

    commmm

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ public class Main
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World! All" );
         System.out.println("What's up mate?");
     }
 }
